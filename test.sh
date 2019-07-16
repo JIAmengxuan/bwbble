0d3afa25c9bd021d284c9617bdfdc9a6937d9e8f
JIAmenngxuan <1092236342@qq.com>
2019-07-16 03:25:24 +0000

    modified the script

--- test.sh
+++ test.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 echo "TEST BEGIN" | tee omp_vs_tp_log.txt
-for i in 1 2 4 6 8
+for i in 1 2 4 6 8 10 12 14 16
 do
 	echo "-----------------------------------\nROUND $i BEGIN" | tee -a omp_vs_tp_log.txt
 	echo "===================================\nTest OMP with $i Threads begin" | tee -a omp_vs_tp_log.txt
-	mg-aligner/build/bwbble align -t $i test_data/GCA_000001405.28_GRCh38.p13_genomic.fna test_data/reads5000000.fq mg-aligner/build/output/reads5000000.aln | tee -a omp_vs_tp_log.txt
+	mg-aligner/build/bwbble align -t $i test_data/GCA_000001405.28_GRCh38.p13_genomic.fna test_data/reads10000000.fq mg-aligner/build/output/reads10000000.aln | tee -a omp_vs_tp_log.txt
 	echo "Test OMP end\n===================================" | tee -a omp_vs_tp_log.txt
 	echo "===================================\nTest TP with $i Threads begin" | tee -a omp_vs_tp_log.txt
-	mg-aligner/newBuild/bwbble align -t $i test_data/GCA_000001405.28_GRCh38.p13_genomic.fna test_data/reads5000000.fq mg-aligner/newBuild/output/reads5000000.aln | tee -a omp_vs_tp_log.txt
+	mg-aligner/newBuild/bwbble align -t $i test_data/GCA_000001405.28_GRCh38.p13_genomic.fna test_data/reads10000000.fq mg-aligner/newBuild/output/reads10000000.aln | tee -a omp_vs_tp_log.txt
 	echo "Test TP end\n===================================" | tee -a omp_vs_tp_log.txt
 	echo "ROUND $i END\n-----------------------------------" | tee -a omp_vs_tp_log.txt
 done
